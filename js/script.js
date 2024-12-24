d951ec25536a715c069985f3687c71c9d6a9b1ee
Lauren Guardala <laurensiminski@gmail.com>
2024-12-24 13:24:26 -0500

    Cleaned up

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
-(function($){
+(function($) {
     $.fn.extend({ 
         rotaterator: function(options) {
- 
-            var defaults = {
+            let defaults = {
                 fadeSpeed: 500,
                 pauseSpeed: 1800,
 				child:null
-            };
-             
-            var options = $.extend(defaults, options);
-         
+            };            
+            var options = $.extend(defaults, options);         
             return this.each(function() {
-                  var o =options;
-                  var obj = $(this);                
-                  var items = $(obj.children(), obj);
-				  items.each(function() {$(this).hide();})
-				  if(!o.child){var next = $(obj).children(':first');
-				  }else{var next = o.child;
-				  }
-				  $(next).fadeIn(o.fadeSpeed, function() {
-						$(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
-							var next = $(this).next();
-							if (next.length == 0){
-									next = $(obj).children(':first');
-							}
-							$(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
-						})
+                let o = options;
+                let obj = $(this);                
+                let items = $(obj.children(), obj);
+				items.each(function() {$(this).hide();})
+				if (!o.child) {
+					var next = $(obj).children(':first');
+				} else {
+					var next = o.child;
+				}
+				$(next).fadeIn(o.fadeSpeed, function() {
+					$(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
+						let next = $(this).next();
+						if (next.length == 0){
+							next = $(obj).children(':first');
+						}
+						$(obj).rotaterator({
+							child : next, 
+							fadeSpeed : o.fadeSpeed, 
+							pauseSpeed : o.pauseSpeed
+						});
 					});
+				});
             });
         }
     });
 })(jQuery);
-$(document).ready(function() {
-	$('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:1800});
+
+$(document).ready(() => {
+	$('#rotate').rotaterator({
+		fadeSpeed: 500, 
+		pauseSpeed: 1800
+	});
 });	
